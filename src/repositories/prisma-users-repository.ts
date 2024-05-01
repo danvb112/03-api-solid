1330c32b2d2c916445b3a6ba3656ba239d0fddfe
Daniel Bonasser <danielbonasser@gmail.com>
2024-04-30 21:17:23 -0300

    Create UserRepository interface

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -1,13 +1,23 @@
 import { prisma } from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
+import { UsersRepository } from './users-repository';
 
-
-export class PrismaUsersRepository {
+export class PrismaUsersRepository implements UsersRepository {
     async create(data: Prisma.UserCreateInput) {
         const user = await prisma.user.create({
             data,
         });
 
         return user;
+    }
+
+    async findUserByEmail(email: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            }
+        });
+
+        return user
     }
 }
--- src/useCases/register.ts
+++ src/useCases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 
 interface RegisterUseCaseParams {
@@ -8,7 +8,7 @@ interface RegisterUseCaseParams {
 }
 
 export class RegisterUseCase {
-    constructor(private usersRepository: any) { }
+    constructor(private usersRepository: UsersRepository) { }
 
     async execute({
         email,
@@ -17,11 +17,7 @@ export class RegisterUseCase {
     }: RegisterUseCaseParams) {
         const password_hash = await hash(password, 6);
 
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email,
-            }
-        });
+        const userWithSameEmail = await this.usersRepository.findUserByEmail(email);
 
         if (userWithSameEmail) {
             throw new Error('User already exists.')
