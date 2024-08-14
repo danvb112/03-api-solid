3bccbaec822520721731d11617ca81e1b4b90cdb
Daniel Bonasser <danielbonasser@gmail.com>
2024-08-14 00:31:14 +0000

    Apply findUserIdOnDate on CheckInUseCase

--- src/useCases/check-in.ts
+++ src/useCases/check-in.ts
@@ -17,6 +17,15 @@ export class CheckInUseCase {
         gymId,
         userId,
     }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+        const checkInOnSameDate = await this.checkInsRepository.findUserIdOnDate(
+            userId,
+            new Date()
+        );
+
+        if (checkInOnSameDate) {
+            throw new Error();
+        }
+
         const checkIn = await this.checkInsRepository.create({
             gym_id: gymId,
             user_id: userId,
