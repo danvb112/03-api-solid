b35c9fdf341cf7e09708664650533fc176889018
Daniel Bonasser <danielbonasser@gmail.com>
2024-08-14 00:30:45 +0000

    Create findUserIdOnDate function in check-ins-repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,4 +2,5 @@ import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+    findUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -2,7 +2,19 @@ import { CheckIn, Prisma } from "@prisma/client";
 import { CheckInsRepository } from "../check-ins-repository";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
-    public items: CheckIn[] = []
+    public items: CheckIn[] = [];
+
+    async findUserIdOnDate(userId: string, date: Date) {
+        const checkInOnSameDate = this.items.find(
+            (checkIn) => checkIn.user_id === userId
+        );
+
+        if (!checkInOnSameDate) {
+            return null;
+        }
+
+        return checkInOnSameDate;
+    }
 
     async create(data: Prisma.CheckInUncheckedCreateInput) {
         const checkIn = {
